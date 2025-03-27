21572c53ab78004fc38d6ef467ed37555280be05
Allenfu-code <dog4891072.gmail.com>
2025-03-27 23:43:37 +0800

    Test45

--- Test.py
+++ Test.py
@@ -4,6 +4,6 @@ res = requests.get(
     "http://norway.twsthr.info/StockHolders.aspx?stock=" + sid + "&fbclid=IwAR0Hq4oVDTnoj8d-jFlomkoLIPiGFM6HiioDDaWeb-KREDwhmXZiuRCtMdk")
 
 # res = requests.get("你的目標網址")
-with open("debug.html", "w", encoding='utf-8') as f:
+with open("./debug.html", "w", encoding='utf-8') as f:
     f.write(res.text)
 print(res.text)
